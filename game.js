700caaa8f1fa2367904e878adbcf629059370c21
jobilk <bilk889@gmail.com>
2025-01-03 08:19:47 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const apiUrl = `${localIp}/get_balance/${userId}`;
     let userBalance = 0;
 
-    async function fetchBalance() {
+	async function fetchBalance() {
 		try {
 			const response = await fetch(apiUrl, {
 				headers: {
-					'ngrok-skip-browser-warning': 'true'
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 				}
 			});
 			const data = await response.json();
